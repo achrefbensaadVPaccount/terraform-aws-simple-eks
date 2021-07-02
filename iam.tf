451b6c5b4addcf6fa4f9e6692d851d3814433600
Achref Ben Saad <achref.bensaad@cimpress.com>
2021-07-02 06:37:01 -0400

    Code review

--- iam.tf
+++ iam.tf
@@ -19,13 +19,11 @@ resource "aws_iam_role" "role" {
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.role.name
-  tags       = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.role.name
-  tags       = var.tags
 }
 
 resource "aws_iam_role" "worker_role" {
@@ -47,19 +45,16 @@ resource "aws_iam_role" "worker_role" {
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.worker_role.name
-  tags       = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.worker_role.name
-  tags       = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.worker_role.name
-  tags       = var.tags
 }
 
 # Allow workers to send logs to CloudWatch
@@ -67,7 +62,6 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
 resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   role       = aws_iam_role.worker_role.name
-  tags       = var.tags
 }
 
 # Allow workers to modify instance attributes (needed for Calico CNI)
@@ -95,5 +89,4 @@ resource "aws_iam_role_policy" "modify_instance_attributes" {
     ]
   })
 
-  tags = var.tags
 }
